b207a7c71e89a152c7c3892ef137d4cb42df09d9
Tyler Fedoris <tylerfedoris@outlook.com>
2021-12-19 22:19:19 -0500

    -Created Input Actions Asset -Added Jump input action -Implemented Jump method in the PlayerController script

---
+++ Assets/FlappyBirdClone/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace FlappyBirdClone.Scripts
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private float jumpStrength = 20f;
+        [SerializeField] private float maxVelocity = 10.0f;
+
+        private PlayerControls controls;
+        private new Rigidbody2D rigidbody2D;
+
+        private void Awake()
+        {
+            controls = new PlayerControls();
+            controls.PlayerActions.Jump.performed += ctx => Jump();
+        }
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            Debug.Log(rigidbody2D.velocity.magnitude);
+        }
+
+        private void Jump()
+        {
+            if (rigidbody2D.velocity.y < maxVelocity)
+            {
+                rigidbody2D.AddForce(transform.up * jumpStrength, ForceMode2D.Impulse);   
+            }
+        }
+
+        private void OnEnable()
+        {
+            controls.PlayerActions.Enable();
+        }
+        
+        private void OnDisable()
+        {
+            controls.PlayerActions.Disable();
+        }
+    }
+}
