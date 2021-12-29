50663796bdc3f63613d5db8f36d42bf266a604a1
Tyler Fedoris <tylerfedoris@outlook.com>
2021-12-29 00:44:39 -0500

    -Combined all background prefabs into a single prefab -Changed Background system to change out sprites instead of prefabs

--- Assets/FlappyBirdClone/Scripts/Background.cs
+++ Assets/FlappyBirdClone/Scripts/Background.cs
@@ -6,16 +6,63 @@ namespace FlappyBirdClone.Scripts
 {
     public class Background : MonoBehaviour
     {
-        [SerializeField] private List<GameObject> backgroundPrefabs;
-        [SerializeField] private Vector3 startingPosition = Vector3.zero;
-        [SerializeField] private float xOffset;
-        [SerializeField] private int sections = 3;
-        
-        private GameObject[] backgroundSections;
+        [System.Serializable]
+        private struct BackgroundSpriteObject
+        {
+            public Sprite top;
+            public Sprite mid;
+            public Sprite bottom;
+        }
+
+        [System.Serializable]
+        private struct BackgroundObject
+        {
+            public GameObject top;
+            public GameObject mid;
+            public GameObject bottom;
+        }
+
+        [SerializeField] private List<BackgroundSpriteObject> backgroundSprites;
+        [SerializeField] private float xOffset = 10.5f;
+        [SerializeField] private int backgroundIndex = 0;
+
+        [SerializeField] private BackgroundObject SectionA;
+        [SerializeField] private BackgroundObject SectionB;
 
         private void Awake()
         {
-            backgroundSections = new GameObject[sections];
+            if (backgroundIndex > backgroundSprites.Count)
+            {
+                return;
+            }
+            
+            // Section A Sprites
+            foreach (var spriteRenderer in SectionA.top.GetComponentsInChildren<SpriteRenderer>())
+            {
+                spriteRenderer.sprite = backgroundSprites[backgroundIndex].top;
+            }
+            foreach (var spriteRenderer in SectionA.mid.GetComponentsInChildren<SpriteRenderer>())
+            {
+                spriteRenderer.sprite = backgroundSprites[backgroundIndex].mid;
+            }
+            foreach (var spriteRenderer in SectionA.bottom.GetComponentsInChildren<SpriteRenderer>())
+            {
+                spriteRenderer.sprite = backgroundSprites[backgroundIndex].bottom;
+            }
+            
+            // Section B Sprites
+            foreach (var spriteRenderer in SectionB.top.GetComponentsInChildren<SpriteRenderer>())
+            {
+                spriteRenderer.sprite = backgroundSprites[backgroundIndex].top;
+            }
+            foreach (var spriteRenderer in SectionB.mid.GetComponentsInChildren<SpriteRenderer>())
+            {
+                spriteRenderer.sprite = backgroundSprites[backgroundIndex].mid;
+            }
+            foreach (var spriteRenderer in SectionB.bottom.GetComponentsInChildren<SpriteRenderer>())
+            {
+                spriteRenderer.sprite = backgroundSprites[backgroundIndex].bottom;
+            }
         }
 
         // Start is called before the first frame update
@@ -23,11 +70,6 @@ namespace FlappyBirdClone.Scripts
         {
             var thisTransform = transform;
             var thisRotation = thisTransform.rotation;
-
-            for (var i = 0; i < backgroundSections.Length; ++i)
-            {
-                backgroundSections[i] = Instantiate(backgroundPrefabs[0], startingPosition + new Vector3(xOffset * i, 0f, 0f), thisRotation, thisTransform);
-            }
         }
 
         // Update is called once per frame
