7f2c3f711efd96baeb977335c63e269c1c93489d
Tyler Fedoris <tylerfedoris@outlook.com>
2021-12-31 23:16:07 -0500

    -Created ground object -Laid ground work in GameEvents to facilitate the ability to change seasons and time of day

--- Assets/FlappyBirdClone/Scripts/GameEvents.cs
+++ Assets/FlappyBirdClone/Scripts/GameEvents.cs
@@ -6,8 +6,28 @@ namespace FlappyBirdClone.Scripts
 {
     public class GameEvents : MonoBehaviour
     {
+        public enum Season
+        {
+            Summer,
+            Winter
+        }
+
+        public enum TimeOfDay
+        {
+            Day,
+            Sunset,
+            Evening,
+            Night,
+            Twilight
+        }
+
+        private Season[] seasons;
+        private TimeOfDay[] timesOfDay;
+
         public static GameEvents Current;
         public bool GameOver = false;
+        public Season CurrentSeason = Season.Summer;
+        public TimeOfDay CurrentTimeOfDay = TimeOfDay.Day;
 
         private void Awake()
         {
@@ -29,6 +49,29 @@ namespace FlappyBirdClone.Scripts
             Debug.Log("GAME OVER!");
             GameOver = true;
             onTriggerGameOver?.Invoke();
+        }
+
+        public void IncrementSeason()
+        {
+            CurrentSeason = CurrentSeason switch
+            {
+                Season.Summer => Season.Winter,
+                Season.Winter => Season.Summer,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+        
+        public void IncrementTimeOfDay()
+        {
+            CurrentTimeOfDay = CurrentTimeOfDay switch
+            {
+                TimeOfDay.Day => TimeOfDay.Sunset,
+                TimeOfDay.Sunset => TimeOfDay.Evening,
+                TimeOfDay.Evening => TimeOfDay.Night,
+                TimeOfDay.Night => TimeOfDay.Twilight,
+                TimeOfDay.Twilight => TimeOfDay.Day,
+                _ => throw new ArgumentOutOfRangeException()
+            };
         }
     }
 }
